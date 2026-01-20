808f2e9c7b754d003e5fde34934cf5957b4e179a
Martin West <martin@tinarmengineering.com>
2026-01-20 23:51:19 +0100

    minimal changes to make it installable

---
+++ src/geometry/__init__.py
@@ -0,0 +1,20 @@
+from geometric_primitives.arc import Arc
+from geometric_primitives.point import Point
+from geometric_primitives.segment import Segment
+
+
+class Geometry:
+    def __init__(self):
+        self.segments = []
+        self.arcs = []
+        # self.circles = []
+
+    def clear(self):
+        self.segments = []
+        self.arcs = []
+
+    def remove_entity(self, entity):
+        if isinstance(entity, Segment):
+            self.segments.remove(entity)
+        elif isinstance(entity, Arc):
+            self.arcs.remove(entity)
